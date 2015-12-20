797796e7b950357adcdd958c9aff8832e0ff3ae9
Daniel Azuma <dazuma@gmail.com>
2015-12-20 08:38:30 -0800

    Try supporting elixir 1.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Erl2ex.Mixfile do
   def project do
     [app: :erl2ex,
      version: "0.0.1",
-     elixir: "~> 1.1",
+     elixir: "~> 1.0",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      escript: [main_module: Erl2ex.Cli],
